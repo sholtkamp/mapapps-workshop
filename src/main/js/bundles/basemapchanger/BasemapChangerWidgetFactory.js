274789f1492389a27cc1912c8768a7e046f05d42
Matthias Stein <m.stein@conterra.de>
2020-01-14 01:13:10 +0100

    Add code changes from task6

--- src/main/js/bundles/basemapchanger/BasemapChangerWidgetFactory.js
+++ src/main/js/bundles/basemapchanger/BasemapChangerWidgetFactory.js
@@ -3,9 +3,26 @@ import Vue from "apprt-vue/Vue";
 import VueDijit from "apprt-vue/VueDijit";
 import BasemapChangerWidget from "./BasemapChangerWidget.vue";
 
+const _vm = Symbol("_vm");
+const _binding = Symbol("_binding");
+
 export default class BasemapChangerWidgetFactory {
 
+    activate() {
+        this._initComponent();
+    }
+
+    deactivate() {
+        this[_binding].unbind();
+        this[_binding] = undefined;
+        this[_vm] = undefined;
+    }
+
     createInstance() {
+        return VueDijit(this[_vm]);
+    }
+
+    _initComponent() {
         const basemapsModel = this._basemapsModel;
         const basemaps = basemapsModel.basemaps.map((basemap) => {
             return {
@@ -14,15 +31,14 @@ export default class BasemapChangerWidgetFactory {
             }
         });
 
-        let vm = new Vue(BasemapChangerWidget);
+        const vm = this[_vm] = new Vue(BasemapChangerWidget);
 
-        Binding.for(vm, basemapsModel)
+        this[_binding] = Binding.for(vm, basemapsModel)
             .syncAll("selectedId")
             .syncAllToLeft("basemaps")
             .syncToLeftNow()
             .enable();
-
-        return VueDijit(vm);
     }
+
 
 }
