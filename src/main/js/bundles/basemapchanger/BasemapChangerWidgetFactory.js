8168b152953f2bf103e1c2ff9fe692b2aefc66e3
Matthias Stein <m.stein@conterra.de>
2020-12-15 15:31:21 +0100

    Remove unused binding part

--- src/main/js/bundles/basemapchanger/BasemapChangerWidgetFactory.js
+++ src/main/js/bundles/basemapchanger/BasemapChangerWidgetFactory.js
@@ -39,7 +39,6 @@ export default class BasemapChangerWidgetFactory {
 
         this.#binding = Binding.for(vm, basemapsModel)
             .syncAll("selectedId")
-            .syncAllToLeft("basemaps")
             .syncToLeftNow()
             .enable();
     }
